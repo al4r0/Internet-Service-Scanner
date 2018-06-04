56b0facc20ebf58cb6aa2e4de3e990e93cf11ddf
Álvaro Ramos <v4char@gmail.com>
2018-06-04 16:42:29 +0200

    Update respuesta.py

--- respuesta.py
+++ respuesta.py
@@ -2,8 +2,8 @@ CRLF = "\r\n\r\n"
 
 def enviar(puerto):
    if (puerto == 80):
-      return "HEAD / HTTP/1.0%s" % (CRLF)
+      return "GET / HTTP/1.0%s" % (CRLF)
    elif (puerto == 8080):
-      return "HEAD / HTTP/1.0%s" % (CRLF)
+      return "GET / HTTP/1.0%s" % (CRLF)
    else:
       return CRLF
